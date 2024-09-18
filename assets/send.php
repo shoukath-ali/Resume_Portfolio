4ef2e36722b0c7941ed0e2b261e048aa52693823
Shoukath Ali Shaik <shoukath.vizag@gmail.com>
2024-09-17 20:46:16 -0400

    Update Major

---
+++ assets/send.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = 'shshaik@iu.edu'; // Replace with your email address
+    $subject = 'Contact Form Submission --- FROM PORTFOLIO';
+    $body = "Name: $name\nEmail: $email\nMessage:\n$message";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
